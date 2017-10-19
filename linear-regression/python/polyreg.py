d79259e2830baba230c8add6e0381fec3a34054c
justin.noel@mathematik.uni-regensburg.de <justin.noel@mathematik.uni-regensburg.de>
2017-10-19 14:29:21 +0200

    Cleaned up and added comments.

--- linear-regression/python/polyreg.py
+++ linear-regression/python/polyreg.py
@@ -1,3 +1,4 @@
+# This file implements polynomial regression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 import numpy as np
@@ -5,36 +6,34 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from sklearn.metrics import mean_squared_error
 
+#Set number of samples and seed
 NUM_SAMPLES = 100
 np.random.seed(42)
 
+# Our `True' function
 def f(x): 
     return 7 *(x**3 -1.3 *x**2+0.5*x - 0.056)
+
+# initialize sample data
 data = np.array([[x,f(x) ] for x in np.random.random(NUM_SAMPLES)])
-orig_data = np.copy(data)
-# data[:,1] += 0.3*np.random.randn(NUM_SAMPLES)
+
+# grid of coordinates for true function
 gridx = np.linspace(0, 1, NUM_SAMPLES)
 gridy = np.array([f(x) for x in gridx])
 
 datax = data[:,0]
 normaly = data[:,1]+0.3*np.random.randn(NUM_SAMPLES)
 
-plt.scatter(data[:,0], normaly )
+#Plot sampled data points
+plt.scatter(datax, normaly )
 plt.title("Scatter plot of synthetic data with normal errors")
 plt.plot(gridx, gridy, label = "True function", color = 'Red')
 plt.legend(loc = 2)
 plt.savefig("poly_scatter_normal.png")
 plt.cla()
 
-# laplacey = data[:,1]+np.random.laplace(scale = 0.3/np.sqrt(2), size = NUM_SAMPLES)
-# plt.scatter(data[:,0], laplacey)
-# plt.title("Scatter plot of synthetic data with Laplacian errors")
-# plt.plot(gridx, gridy, label = "True function", color = 'Red')
-# plt.legend(loc = 2)
-# plt.savefig("scatter_laplace.png")
-# plt.cla()
-
 gen_poly = True
+# Run polynomial regression repeatedly for increasing degrees
 if gen_poly:
     lm = LinearRegression()
     for deg in range(1, 8):
@@ -43,8 +42,9 @@ if gen_poly:
         for i in range(1, NUM_SAMPLES+1):
             lm.fit(newdatax[:i], normaly[:i].reshape(i, 1))
             predictions = lm.predict(newdatax)
-            # print(predictions)
             mse = mean_squared_error(predictions, normaly.reshape(NUM_SAMPLES,1))
+
+            #Plot everything
             plt.ylim(-0.75, 1.25)
             plt.scatter(datax, normaly)
             plt.title("Degree {} polynomial regression on {} points with normal error".format(deg, i))
@@ -55,7 +55,8 @@ if gen_poly:
             plt.savefig("polyreg_normal_{:02d}{:03d}.png".format(deg,i))
             plt.cla()   
 
-gen_var = False
+# Run degree 10 polynomial regression repeatedly using a random sample of 30 points
+gen_var = True
 if gen_var:
     lm = LinearRegression()
     poly = PolynomialFeatures(degree = 10)
@@ -64,8 +65,9 @@ if gen_var:
         samp = np.random.choice(range(NUM_SAMPLES), 30)
         lm.fit(newdatax[samp], normaly[samp].reshape(30, 1))
         predictions = lm.predict(newdatax)
-        # print(predictions)
         mse = mean_squared_error(predictions, normaly.reshape(NUM_SAMPLES,1))
+
+        #Plot everything 
         plt.ylim(-0.75, 1.25)
         plt.scatter(datax, normaly)
         plt.title("Degree {} polynomial regression on 30 random points with normal error".format(10))
@@ -75,15 +77,3 @@ if gen_var:
         plt.legend(loc = 2)
         plt.savefig("polyreg_var_{:03d}.png".format(i))
         plt.cla()   
-# lm = LinearRegression()
-# for i in range(1, NUM_SAMPLES+1):
-#     lm.fit(datax[:i].reshape((i,1)), laplacey[:i].reshape((i,1)))
-#     predictions = lm.predict(datax.reshape(NUM_SAMPLES,1))
-#     mse = mean_squared_error(predictions, laplacey)
-#     plt.scatter(datax, normaly)
-#     plt.title("Linear regression on {} points with Laplacian error".format(i))
-#     plt.plot(gridx, gridy, label = "True function", color = 'Red')
-#     plt.plot(gridx, [lm.coef_[0] * x + lm.intercept_[0]  for x in gridx], label = "Linear regressor line MSE = {:0.4f}".format(mse), color = 'Green')
-#     plt.legend(loc = 2)
-#     plt.savefig("linreg_laplace_{:03d}.png".format(i))
-#     plt.cla()   
